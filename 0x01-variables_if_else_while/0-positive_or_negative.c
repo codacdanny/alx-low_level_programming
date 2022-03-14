b85e36aa68cbb3dc82eb3c406ced5eaa5a2b62f3
codacdanny <danpresh302@gmail.com>
2022-03-14 02:41:26 -0700

    added betty format

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-<<<<<<< HEAD
 
 /* more headers goes there */
 /**
@@ -9,17 +8,7 @@
  *takes input n and checks it
  *Return: Always 0
  *(Success)
-=======
-#include <stdio.h>
-
-/* more headers goes there */
-/**
- * main - function that checks positive 
- * and negative numbers
- * Return: Always 0
- * (Succes)
->>>>>>> 3158860ffd61eeda9707dfd0d59d4691625c95e0
- */
+*/
 /* betty style doc for function main goes there */
 
 int main(void)
@@ -31,23 +20,10 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-<<<<<<< HEAD
 		printf("%d is positive\n", n);
 	} else if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else
-	{
-		printf("%d is zero\n", n);
-=======
-		printf("%d is positive\n",n);
-	} elseif (n < 0)
-	{
-		printf("%d is negative\n",n);
-	} else
-	{
-		printf("%d is zero\n",n);
->>>>>>> 3158860ffd61eeda9707dfd0d59d4691625c95e0
 	}
 
 	return (0);
