822280538e862484b0125e4001e4b5519dedb0bc
codacdanny <danpresh302@gmail.com>
2022-03-13 21:58:01 -0700

    checked and updated betty format

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-
+/*function to determine positive and negative integer*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -11,11 +11,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0){
+	if (n > 0) 
+	{
 		printf("%d is positive\n", n );
-	} else if (n < 0) {
+	} else if (n < 0) 
+	{
 		printf("%d is negative\n", n);
-	} else {
+	} else 
+	{
 		printf("%d is zero\n", n);
 	}
 
