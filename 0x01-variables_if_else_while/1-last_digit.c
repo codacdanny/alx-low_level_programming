9b9bcfa3e5bfac092a2632258024437f94757971
codacdanny <danpresh302@gmail.com>
2022-03-15 10:25:52 +0000

    debugged with betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio>
+#include <stdio.h>
 /* more headers goes there */
 /** 
 *main -this prints the last
@@ -8,7 +8,6 @@
 * Return: Always return 0 (Success)
 */
 /* betty style doc for function main goes there */
-
 int main(void)
 {
 	int n;
@@ -20,8 +19,9 @@ int main(void)
 	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
-		printf("and is 0\n)
-	else if (n % 10 < 6 && n % 10 != 0);
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
 
