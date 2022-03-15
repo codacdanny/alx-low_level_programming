18ae3acf2ed6009be2c6fa2c4971a3cd3d665b5a
codacdanny <danpresh302@gmail.com>
2022-03-15 10:33:11 +0000

    second attempt to remove trailing white space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /* more headers goes there */
 /** 
-*main -this prints the last
+*main - this prints the last
 * digit of the inputed value
 * Return: Always return 0 (Success)
 */
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("Last digit of %d is %d", n, n % 10);
+	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
