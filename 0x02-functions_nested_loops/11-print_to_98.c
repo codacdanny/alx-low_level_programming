351ae68d6fe828f94d094f7d5b57af2edb46f72c
codacdanny <danpresh302@gmail.com>
2022-03-18 15:54:03 +0000

    counting to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - count from n to 98
+ * @n: integer
+ * Return: always 0.
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
+}
+
