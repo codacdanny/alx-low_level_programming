7b9f2f4cdd619771bee582482363ff9249ae2d2d
codacdanny <danpresh302@gmail.com>
2022-03-18 08:18:58 +0000

    printing all possible combo of two digits

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to the stdout
- * @c: the character to print
- *
- * Return: On succcess 1.
- * on error, -1 is returned, and errno is set appropriately.
+ * main - prints -_putchar, fllowed by a new line
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
 
